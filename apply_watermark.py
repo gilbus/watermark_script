1683d8cd3f04640cc73d858647cd40c35f0837dd
tluettje <tilman@fachschaft.techfak.de>
2018-12-03 14:34:25 +0100

    rewrite most of the code, add possibility of local config

--- apply_watermark.py
+++ apply_watermark.py
@@ -1,63 +1,74 @@
 #!/usr/bin/env python3
-
 """
-Ein kleines Programm, welches ein PDF-Dokument mit einem anderen signiert.
+Dieses Skript nimmt ein oder mehrere digitale Dokumente im PDF-Format entgegen und
+versieht sie mit dem Fachschafts-Wasserzeichen auf allen Seiten.
+
+Das Wasserzeichen kann bei Wunsch frei gewählt werden. Für das Wasserzeichen wird das
+Programm `pdftk` benötigt, für den optionalen GUI-Modus `zenity`. Die wichtigsten
+Optionen können in der Kommandozeilen-Version oder mithilfe einer Konfigurationsdatei
+namens `config.toml` im gleichen Ordner wie dieses Skript geändert werden.
 """
-import sys
-import subprocess
-import argparse
-import shutil
-import os
-from typing import List
+from pathlib import Path
+from subprocess import run, STDOUT, PIPE as CAPTURE, CalledProcessError
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from shutil import which
+from collections import ChainMap
 
-__license__ = 'AGPLv3 or later'
-__author__ = 'tluettje'
-__version__ = '1.0'
+try:
+    import toml
+except ImportError:
+    print("This script requires python(3?)-toml, please install it")
+    exit(1)
 
-DEFAULT_WATERMARK_PDF = '/vol/fachschaft/share/MBT/watermark_script/fs_watermark.pdf'  # type: str
-
-OUT_FOLDER = '/vol/fachschaft/share/MBT/'
-
-ZENITY_PATH = None  # type: str
-
-PDFTK_PATH = None  # type: str
-
-EXPLANATION = """
-Dieses Skript nimmt ein oder mehrere digitale Dokumente im PDF-Format entgegen und versieht sie mit dem
-Fachschafts-Wasserzeichen (standardmäßig <b>%s</b>) auf allen Seiten.
-Das Wasserzeichen kann bei Wunsch frei gewählt werden. Für das Wasserzeichen wird das Programm
-`pdftk` benötigt, für den optionalen GUI-Modus `zenity`.
-""" % DEFAULT_WATERMARK_PDF
-
-GUI_TEXT = """
-Dieser GUI-Modus fragt nur nach den PDF-Dateien und wendet das Standard-Wasserzeichen an, die neuen
-Dateien wird unter <b><i>OriginalDateiName</i>_watermark.pdf</b> gespeichert. Auf der Kommandozeile
-kann auch ein anderes Wasserzeichen angegeben werden.
-"""
-
+__license__ = "AGPLv3 or later"
+__author__ = "tluettje"
+__version__ = "1.0"
+__url__ = "github.com/gilbus/watermark_script"
+# {{{ messages
 ZENITY_NOT_INSTALLED = """
-Das Programm <b>zenity</b> ist nicht installiert, wird für den GUI-Modus aber benötigt. Alternativ kann
-dieses Skript auch auf der Kommandozeile benutzt werden.
+Das Programm `zenity` ist nicht installiert, wird für den GUI-Modus aber benötigt.
+Alternativ kann dieses Skript auch auf der Kommandozeile benutzt werden.
 """
 
+# HTML like markup is rendered inside zenity
 PDFTK_NOT_INSTALLED = """
-Das Programm <b>pdftk</b> ist nicht installiert, wird aber benötigt um das Wasserzeichen anzuwenden.
+Das Programm <b>pdftk</b> ist nicht installiert, wird aber benötigt um das Wasserzeichen
+anzuwenden.
 Auf Wiedersehen.
 """
+
+WATERMARK_FILE_ERROR = """
+Beim Öffnen der Datei mit dem Wasserzeichen ist ein Fehler aufgetreten:
+    <span foreground='red'>{error}</span>
+"""
+
+PDF_INPUT_ERROR = """
+Beim Öffnen der PDF-Datei ist ein Fehler aufgetreten:
+    <span foreground='red'>{error}</span>
+"""
+
 
 PDFTK_CALL_ERROR = """
 Bei dem Aufruf von <b>pdftk</b> ist folgender Fehler aufgetreten:
-    <span foreground='red'>{error}</span>
+    <span foreground='red'>{{error}}</span>
 
 Das ursprüngliche Kommando war:
-    <tt>{cmd}</tt>
-Bitte melde dich bei %s für weitere Fragen.
+    <tt>{{cmd}}</tt>
+Bitte melde dich bei {author} bei weiteren Fragen oder öffne ein Issue auf Github
+({url}).
 Auf Wiedersehen.
-""" % __author__
+""".format(
+    author=__author__, url=__url__
+)
 
-PDFTK_UNKNOWN_ERROR = """
-Es ist ein unbekannter Fehler aufgetreten, bitte melde dich bei %s
-""" % __author__
+
+PDFTK_UNKNOWN_ERROR = """Es ist ein unbekannter Fehler aufgetreten:
+<span foreground='red'>{{error}}</span>"""
+
+FILE_SAVE_ERROR = """
+Konnte die produzierte PDF-Datei nicht unter <tt>{path}</tt> speichern. Der Fehler war:
+    <span foreground='red'>{error}</span>
+"""
 
 SIGN_SUCCESS = """
 Es scheint alles funktioniert zu haben. Die signierte Datei wurde unter
@@ -65,111 +76,198 @@ Es scheint alles funktioniert zu haben. Die signierte Datei wurde unter
 gespeichert.
 Einen schönen Tag noch (:
 """
+# }}}
+default_config = {
+    "output_folder": Path(__file__).parent / "out",
+    "watermark_pdf": Path(__file__).parent / "resources" / "fs_watermark.pdf",
+    "zenity_path": which("zenity"),
+    "pdftk_path": which("pdftk"),
+}
 
-DEFAULT_OUT_FILE = '<OriginalDateiName>_watermark.pdf'
+local_config_file = Path(__file__).parent / "config.toml"
 
-ZENITY_CHOOSE_FILE = """
-Bitte wähle in dem nächsten Dialog die zu signierende PDF-Datei aus.
-"""
 
-WRONG_FILE = """
-Entweder wurde keine Datei ausgewählt oder es ist keine valide PDF-Datei.
-Auf Wiedersehen.
-"""
-
-def main():
-    global PDFTK_PATH, DEFAULT_WATERMARK_PDF
-    # make path to watermark absolut
-    DEFAULT_WATERMARK_PDF = os.path.abspath(DEFAULT_WATERMARK_PDF)
-    parser = argparse.ArgumentParser(description=EXPLANATION,
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-f', '--file', help='Digitales PDF-Dokument welches signiert werden soll.',
-                        type=argparse.FileType('r'))
-    parser.add_argument('-w', '--watermark',
-                        help='Einseitiges PDF-Dokument, welches als Wasserzeichen verwendet wird',
-                        default=DEFAULT_WATERMARK_PDF, type=argparse.FileType('r'))
-    parser.add_argument('-o', '--out', help='Name der AusgabeDatei', type=str,
-                        default=DEFAULT_OUT_FILE)
-    parser.add_argument('--launch-gui', help='Startet den GUI-Modus', action='store_true')
-
-    if len(sys.argv) < 2:
-        parser.print_help()
-        sys.exit(1)
-    args = parser.parse_args()
-    PDFTK_PATH = shutil.which('pdftk')
-    if args.launch_gui:
-        gui(not bool(PDFTK_PATH))
-    else:
-        # if default out filename make string empty
-        if args.out:
-            args.out = ""
-        do_watermark(args.file.name, os.path.abspath(args.watermark.name), args.out)
-
-def gui(pdftk_missing: bool):
-    global ZENITY_PATH
-    # we are using zenity as gui
-    ZENITY_PATH = shutil.which('zenity')
-    # check whether zenity was found
-    if not ZENITY_PATH:
-        # xmessage should be installed
-        xmessage_path = shutil.which('xmessage')
-        if not xmessage_path:
-            sys.exit(2)
-        subprocess.call([xmessage_path, ZENITY_NOT_INSTALLED])
-        sys.exit(1)
-    if pdftk_missing:
-        subprocess.call([ZENITY_PATH, '--error', '--text=%s'%PDFTK_NOT_INSTALLED])
-        sys.exit(3)
-    # zenity is available
-    return_code = subprocess.call([ZENITY_PATH, '--info', '--text=%s' % GUI_TEXT])
-    if return_code != 0:
-        sys.exit(1)
-    return_code = subprocess.call([ZENITY_PATH, '--info', '--text=%s' % ZENITY_CHOOSE_FILE])
-    if return_code != 0:
-        sys.exit(1)
+def load_config() -> ChainMap:
+    """
+    Loads a local config file and returns a ChainMap where local config entries shadow
+    their defaults.
+    """
+    config = ChainMap(default_config)
     try:
-        pdf_files = subprocess.check_output(
-            [ZENITY_PATH, '--file-selection', '--multiple', '--file-filter=*.pdf']
-        ).decode('utf-8').replace('\n', '').split('|')
-        print('Received following *.pdf files: {}'.format(pdf_files))
-    except subprocess.CalledProcessError:
-        subprocess.call([ZENITY_PATH, '--error', '--text=%s' % WRONG_FILE])
-        sys.exit(4)
-    do_watermark(pdf_files, gui=True)
+        local_config = toml.loads(local_config_file.read_text())
+        print("Using local config from: ", local_config_file)
+        return config.new_child(local_config)
+    except FileNotFoundError:
+        print(
+            "No local config file at `{}` detected, using global one".format(
+                local_config_file
+            )
+        )
+    except PermissionError:
+        print(
+            "Local config file `{}` found but could not read it. Skipping".format(
+                local_config_file
+            )
+        )
+    except BaseException as e:
+        print("Unexpected error: `{}`. Aborting".format(e))
+        exit(1)
+    return config
 
 
-def do_watermark(pdf_files: List[str], watermark_file: str = DEFAULT_WATERMARK_PDF,
-                 out_file: str = "", gui: bool = False):
-    for pdf_file in pdf_files:
-        pdf_file = os.path.realpath(pdf_file)
-        file_wo_ext, ext = os.path.splitext(pdf_file)
-        out_file = "%s%s_watermark%s" % (OUT_FOLDER, os.path.basename(file_wo_ext), ext)
-        print(pdf_file, file_wo_ext, ext, out_file)
-        call_args = [PDFTK_PATH, pdf_file, 'stamp', os.path.abspath(watermark_file),
-                     'output', out_file]
-        error_msg = ''
-        fail = False
+def main() -> int:
+    config = load_config()
+    parser = ArgumentParser(
+        description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "file",
+        help="Digitale PDF-Dokumente welche signiert werden sollen.",
+        nargs="*",
+        type=Path,
+    )
+    parser.add_argument(
+        "-w",
+        "--watermark",
+        help="Einseitiges PDF-Dokument, welches als Wasserzeichen verwendet wird",
+        default=config["watermark_pdf"],
+        type=Path,
+    )
+    parser.add_argument(
+        "-o",
+        "--output-folder",
+        help="Name des Ausgabe-Ordners",
+        type=Path,
+        default=config["output_folder"],
+    )
+    parser.add_argument(
+        "-g", "--gui", help="Startet den GUI-Modus", action="store_true"
+    )
+    parser.add_argument(
+        "-d",
+        "--dump-default-config",
+        help="Print the default config in TOML format, useful to create a local config.",
+    )
+
+    args = parser.parse_args()
+
+    if args.dump_default_config:
+        print(toml.dumps(default_config))
+        return 0
+    if args.gui:
+        if not config["zenity_path"]:
+            print(ZENITY_NOT_INSTALLED)
+            return 1
         try:
-            # pdftk does not use exit codes to signal any error -.-'
-            # but in case of success the output is empty, therefore we check for it
-            call = subprocess.run(call_args, stderr=subprocess.PIPE)
-        except subprocess.CalledProcessError:
-            fail = True
-            error_msg = PDFTK_UNKNOWN_ERROR
-        if call.returncode == 1 or fail:
-            error_msg = call.stderr.decode('utf-8') if not fail else error_msg
-            if gui:
-                subprocess.call(
-                    [ZENITY_PATH, '--error', '--text=%s' % PDFTK_CALL_ERROR.format(
-                        error=error_msg, cmd=call_args)])
-            else:
-                print(PDFTK_CALL_ERROR.format(error=error_msg, cmd=call_args), file=sys.stderr)
-            sys.exit(6)
-        if gui:
-            subprocess.call([ZENITY_PATH, '--info', '--text=%s' % SIGN_SUCCESS.format(path=out_file)])
+            run(
+                [config["zenity_path"], "--info", "--text={}".format(__doc__)],
+                check=True,
+            )
+        except CalledProcessError:
+            # user closed window instead of clicking OK
+            return 0
+    elif not args.file:
+        parser.print_help()
+        return 0
+
+    def show_error_msg(msg: str) -> None:
+        """
+        If GUI mode is active show the error via zenity and print it always.
+        """
+        print(msg)
+        if args.gui:
+            run([config["zenity_path"], "--error", "--text={}".format(msg)])
+        exit(1)
+
+    if not config["pdftk_path"]:
+        if args.gui:
+            run(
+                [
+                    config["zenity_path"],
+                    "--error",
+                    "--text={}".format(PDFTK_NOT_INSTALLED),
+                ]
+            )
         else:
-            print(SIGN_SUCCESS.format(path=out_file))
+            print(PDFTK_NOT_INSTALLED)
+            return 1
+    try:
+        # quick check whether file exists and is readable
+        args.watermark.open().close()
+    except BaseException as e:
+        error_msg = WATERMARK_FILE_ERROR.format(error=e)
+        show_error_msg(error_msg)
+        return 1
+    if args.gui:
+        try:
+            args.file = [
+                Path(file)
+                for file in run(
+                    [
+                        config["zenity_path"],
+                        "--title='Zu signierende PDF-Dateien auswählen'",
+                        "--file-selection",
+                        "--multiple",
+                        "--file-filter=*.pdf",
+                    ],
+                    check=True,
+                    stdout=CAPTURE,
+                    universal_newlines=True,
+                )
+                .stdout.replace("\n", "")
+                .split("|")
+            ]
+        except CalledProcessError as e:
+            print(
+                """Either user closed window without selection or an error occurred.
+                    Exiting"""
+            )
+            return 0
+    for file in args.file:
+        try:
+            signed_file = watermark_document(file, args.watermark, config["pdftk_path"])
+        except ValueError as e:
+            error_msg = PDF_INPUT_ERROR.format(error=e)
+            show_error_msg(error_msg)
+        except CalledProcessError as e:
+            error_msg = PDFTK_UNKNOWN_ERROR.format(error=e, cmd=e.args)
+            show_error_msg(error_msg)
+
+        output_filename = Path(
+            args.output_folder / (file.stem + "_watermark" + file.suffix)
+        )
+        try:
+            with open(output_filename, "wb") as output_file:
+                output_file.write(signed_file)
+        except BaseException as e:
+            error_msg = FILE_SAVE_ERROR.format(path=output_filename, error=e)
+            show_error_msg(error_msg)
+            return 1
+        success_msg = SIGN_SUCCESS.format(path=output_filename.absolute())
+        if args.gui:
+            run([config["zenity_path"], "--info", "--text=" + success_msg])
+        else:
+            print(success_msg)
+    return 0
 
 
-if __name__=="__main__":
-    main()
+def watermark_document(document: Path, watermark: Path, pdftk: str) -> bytes:
+    try:
+        # quick check whether file exists and is readable
+        document.open().close()
+    except BaseException as e:
+        raise ValueError(e)
+
+    signed_pdf = run(
+        [pdftk, "-", "stamp", str(watermark), "output", "-"],
+        stdin=document.open("br"),
+        stdout=CAPTURE,
+        stderr=CAPTURE,
+        check=True,
+    )
+
+    return signed_pdf.stdout
+
+
+if __name__ == "__main__":
+    exit(main())
